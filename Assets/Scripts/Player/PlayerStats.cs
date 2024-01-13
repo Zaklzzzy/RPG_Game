2e9d10c8759ef3352b180383ba6be5f76a34f804
Zaklzzzy <zaletin123@gmail.com>
2024-01-13 23:26:46 +0300

    feat: add project files

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int HP;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
