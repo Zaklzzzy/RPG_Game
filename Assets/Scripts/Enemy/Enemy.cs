7aaff3f1ac00a2138bb11471cf2f5d53dd159c40
Zaklzzzy <zaletin123@gmail.com>
2024-03-20 11:59:47 +0300

    feat: add shoot disable

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,31 +4,30 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    //AI Navigation
     [Header("AI Navigation")]
     [SerializeField] private NavMeshAgent _agent;
     [SerializeField] private Transform _player;
     [SerializeField] private float movementSpeed = 3.5f;
 
-    //Damage and Health
     [Header("Damage and Health")]
     [SerializeField] private short HP;
     [SerializeField] private float attackSpeed;
     [SerializeField] private short damage;
 
-    //Shoot
     [Header("Shoot")]
     [SerializeField] private Transform point;
     [SerializeField] private Bullet bullet;
 
     //Animation and Particles
 
+    [Header("Damage and Health")]
+    [SerializeField] private bool enableShoot;
 
     private void Awake()
     {
         _agent = GetComponent<NavMeshAgent>();
         _player = FindFirstObjectByType<PlayerController>().transform;
-        Shoot();
+        if(enableShoot) Shoot();
     }
     private void FixedUpdate()
     {
@@ -56,6 +55,6 @@ public class Enemy : MonoBehaviour
     private void Shoot()
     {
         Instantiate(bullet, point.position, point.rotation);
-        Invoke("Shoot",3f);
+        if (enableShoot) Invoke("Shoot",3f);
     }
 }
